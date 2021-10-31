c4fea1eed11d720a4778d663b63f30cad133c668
ivar <ivar110kg@gmail.com>
2021-10-31 17:03:35 +0400

    add post_detail.html

--- game_blog/blog/urls.py
+++ game_blog/blog/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='posts_list'),
+    path('post/<str:slug>/', posts_detail, name='post_detail'),
 ]
--- game_blog/blog/views.py
+++ game_blog/blog/views.py
@@ -5,3 +5,8 @@ from blog.models import Post
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def posts_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
