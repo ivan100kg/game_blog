183c3dd26c887d6e7fba497c9c2ef094860d291d
ivar <ivar110kg@gmail.com>
2021-11-01 20:22:02 +0400

    add model 'Tag'

--- game_blog/blog/models.py
+++ game_blog/blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -13,3 +14,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', kwargs={'slug': self.slug})
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
