0e1356ff654951ae7ea35590200d59eefbd260b0
ivar <ivar110kg@gmail.com>
2021-11-03 17:22:07 +0400

    add 404 if wrong url

--- game_blog/blog/urls.py
+++ game_blog/blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # path('post/<str:slug>/', post_detail, name='post_detail'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail'),
     path('tags/', tags_list, name='tags_list'),
-    path('tag/<str:slug>/', tag_detail, name='tag_detail'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail'),
 ]
--- game_blog/blog/views.py
+++ game_blog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views import View
 
 from blog.models import Post, Tag
@@ -9,22 +9,28 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
-
-
-class PostDetail(View):
-    def get(self, request, slug):
-        post = Post.objects.get(slug__iexact=slug)
-        return render(request, 'blog/post_detail.html', context={'post': post})
-
-
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags.html', context={'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+# def post_detail(request, slug):
+#     post = Post.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+# def tag_detail(request, slug):
+#     tag = Tag.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+class PostDetail(View):
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug__iexact=slug)
+        return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+class TagDetail(View):
+    def get(self, request, slug):
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        return render(request, 'blog/tag_detail.html', context={'tag': tag})
